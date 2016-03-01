df713768b03c4e7e810ad61d0f6fa75016c497ad
Chris Larsen <clarsen801@gmail.com>
2016-03-01 14:34:00 -0700

    add ElasticBeanstalk.Application resource

--- fmc/resources/elasticbeanstalk/application.py
+++ fmc/resources/elasticbeanstalk/application.py
@@ -1,4 +1,38 @@
 from fmc.resources.base import ResourceBase
+from fmc.exceptions import MissingArgument
 
 class Application(ResourceBase):
-    pass
+    def __init__(self, LogicalID=None, ApplicationName=None, Description=None):
+        if not LogicalID:
+            raise MissingArument("LogicalID")
+
+        self.LogicalID = LogicalID
+        self.ApplicationName = ApplicationName
+        self.Description = Description
+        self.Properties = {}
+        self.type = "AWS:::ElasticBeanstalk::Application"
+        self.doc = {
+                "Resources": {
+                    self.LogicalID: {
+                        "Type": self.type,
+                        "Properties": self.Properties
+                        }
+                    }
+                }
+        if self.ApplicationName:
+            self.Properties["ApplicationName"] = self.ApplicationName
+
+        if self.Description:
+            self.Properties["Description"] = self.Description
+
+        if len(self.Properties.keys()) == 0:
+            print self.doc
+            self.doc["Resources"][self.LogicalID].pop("Properties")
+
+    def __repr__(self):
+        return "<ElasticBeanstalk.Application: {0}>".format(
+                self.LogicalID
+                )
+
+    def representation(self):
+        return self.doc
