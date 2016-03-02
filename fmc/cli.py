9a3a98712d9496de44191e51c7f42f7da20c92d8
Chris Larsen <clarsen801@gmail.com>
2016-03-01 18:33:49 -0700

    pass entire stack resource, imported from file, to Client

--- fmc/cli.py
+++ fmc/cli.py
@@ -1,29 +1,58 @@
 import os
 import sys
 import argparse
+import imp
+import json
+
 try:
     import fmc
 except:
     sys.path.append(os.getcwd())
     import fmc
 
-def get_options():
-    args = argparse.ArgumentParser()
-    args.add_argument("--stack", "-s", type=str,
-            help="Full path to Stack to run operations for.")
-    args.add_argument("--validate", action="store_true",
-            help="Validate Stack.")
+client = fmc.client()
 
-    options = args.parse_args()
-    if len(sys.argv) < 2:
-        args.print_help()
-        sys.exit(0)
+def get_options():
+    parent_parser = argparse.ArgumentParser()
+
+    shared_args = argparse.ArgumentParser(add_help=False)
+    shared_args.add_argument("--stack", "-s", type=str, required=True,
+            help="Path to stack file realive to cwd.")
+
+    subparsers = parent_parser.add_subparsers(dest="operation")
+
+    representation = subparsers.add_parser("representation", parents=[shared_args])
+    validate = subparsers.add_parser("validate", parents=[shared_args])
+    create = subparsers.add_parser("create", parents=[shared_args])
+
+    options = parent_parser.parse_args()
 
     return options
 
+def _dumps(doc):
+    return json.dumps(
+            doc,
+            indent=4,
+            sort_keys=True,
+            )
+
 def main():
     options = get_options()
-    print options
+
+    try:
+        stack = imp.load_source("stack", options.stack)
+    except Exception as e:
+        print str(e)
+        sys.exit()
+
+    if options.operation == "representation":
+        print _dumps(client.stack_representation(stack))
+
+    if options.operation == "create":
+        print _dumps(client.create_stack(stack))
+
+    if options.operation == "validate":
+        print _dumps(client.validate_stack(stack))
 
 if __name__ == "__main__":
     main()
--- fmc/client.py
+++ fmc/client.py
@@ -36,9 +36,9 @@ class Client(Base):
         return combined_dict
 
     @classmethod
-    def validate_stack(Class, configs):
+    def validate_stack(Class, stack):
         self = Class()
-        combined_dict = self._combine_dicts(configs)
+        combined_dict = self._combine_dicts(stack.stack)
         serialized_dict = self.serialize(combined_dict)
 
         valid = self._validate_template(
@@ -48,22 +48,32 @@ class Client(Base):
         return valid
 
     @classmethod
-    def create_stack(Class, configs):
+    def create_stack(Class, stack):
         self = Class()
-        combined_dict = self._combine_dicts(configs)
+        combined_dict = self._combine_dicts(stack.stack)
 
         template_valid = self._validate_template(
                 self.serialize(combined_dict)
                 )
 
-        print self.serialize(combined_dict)
+        if template_valid:
+            try:
+                response = self.cf_client.create_stack(
+                        StackName = stack.name,
+                        TemplateBody = self.serialize(combined_dict),
+                        )
+            except Exception as e:
+                print str(e)
+                sys.exit(1)
+
+        return response
 
     @classmethod
-    def delete_stack(Class, stack_name):
+    def delete_stack(Class, stack):
         self = Class()
         try:
             return self.cf_client.delete_stack(
-                    StackName = stack_name
+                    StackName = stack.name
                     )
         except botocore.exceptions.ClientError as e:
             sys.stderr.write("ERROR: {0}".format(str(e)))
@@ -72,6 +82,9 @@ class Client(Base):
             sys.stderr.write(str(e))
             raise
 
-
+    @classmethod
+    def stack_representation(Class, stack):
+        self = Class()
+        return self._combine_dicts(stack.stack)
 
 #! vim: ts=4 sw=4 ft=python expandtab:
