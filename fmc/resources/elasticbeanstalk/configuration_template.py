e0e2b5f7d06b1456bbe74702674aa3bcbdfaba7d
Chris Larsen <clarsen801@gmail.com>
2016-03-01 18:34:21 -0700

    add ElasticBeanstalk.ConfigurationTemplate resource

--- fmc/resources/elasticbeanstalk/configuration_template.py
+++ fmc/resources/elasticbeanstalk/configuration_template.py
@@ -1,4 +1,53 @@
 from fmc.resources.base import ResourceBase
+from fmc.exceptions import MissingArgument
 
 class ConfigurationTemplate(ResourceBase):
-    pass
+    def __init__(self, LogicalID=None, ApplicationName=None, DependsOn=None, **kwargs):
+        if not LogicalID:
+            raise MissingArgument("LogicalID")
+
+        if not ApplicationName:
+            raise MissingArgument("ApplicationName")
+
+        self.type = "AWS::ElasticBeanstalk::ConfigurationTemplate"
+        self.LogicalID = LogicalID
+        self.ApplicationName = ApplicationName
+        self.User_Properties = kwargs
+        self.DependsOn = DependsOn
+        self.Properties = {
+                "Resources":{
+                    self.LogicalID: {
+                        "Properties": self.User_Properties
+                        }
+                    }
+                }
+        self.doc_base = {
+                "Resources": {
+                    self.LogicalID: {
+                        "Type": self.type,
+                        "Properties": {
+                            "ApplicationName": self.ApplicationName,
+                            }
+                        }
+                    }
+                }
+        if self.DependsOn:
+            self.doc_base["Resources"][self.LogicalID]["DependsOn"] = self.DependsOn
+
+
+    def __repr__(self):
+        return "<ElasticBeanstalk.ConfigurationTemplate: {0}".format(
+                self.LogicalID
+                )
+
+    def representation(self):
+        if self.User_Properties:
+            doc = self._update_dict(
+                    self.doc_base,
+                    self.Properties,
+                    )
+        else:
+            doc = self.doc_base
+
+        return doc
+
