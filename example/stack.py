cc5d463d6c0b25d102f253269e7872a23067642e
Chris Larsen <clarsen801@gmail.com>
2016-03-02 21:59:52 -0700

    update example/stack.py

--- example/stack.py
+++ example/stack.py
@@ -6,6 +6,8 @@ except:
     sys.path.append(os.getcwd())
     import fmc
 
+name = "SampleFMCStack"
+
 description = fmc.description("Sample FMC Stack")
 metadata = fmc.metadata(
         Instances = {
@@ -18,21 +20,56 @@ metadata = fmc.metadata(
 
 eb = fmc.resource("ElasticBeanstalk")
 eb_app = eb.Application(
-        LogicalID = "test-app",
+        LogicalID = "testapp",
         ApplicationName = "Test Application",
         Description = "A Test Application",
         )
 
+eb_template = eb.ConfigurationTemplate(
+        LogicalID = "testebtemplate",
+        ApplicationName = {
+            "Ref": eb_app.LogicalID,
+            },
+        SolutionStackName = "64bit Amazon Linux 2015.09 v2.0.8 running Docker 1.9.1",
+        DependsOn = [
+            eb_app.LogicalID,
+            ],
+        )
+
 eb_env = eb.Environment(
-        LogicalID = "dev-eb-env",
-        ApplicationName = eb_app.LogicalID,
+        LogicalID = "devebenv",
+        Description = "Test Environment",
+        EnvironmentName = "TestEnvironment",
+        ApplicationName = {
+            "Ref": eb_app.LogicalID,
+            },
+        DependsOn = [
+            eb_app.LogicalID,
+            eb_template.LogicalID,
+            ],
+        TemplateName = {
+            "Ref": eb_template.LogicalID,
+            },
+        OptionSettings = [
+            {
+                "Namespace": "aws:autoscaling:launchconfiguration",
+                "OptionName": "InstanceType",
+                "Value": "t2.nano",
+                },
+            {
+                "Namespace": "aws:elasticbeanstalk:environment",
+                "OptionName": "EnvironmentType",
+                "Value": "SingleInstance",
+                },
+            ]
         )
 
 stack = [
         description,
         metadata,
-        eb_app,
         eb_env,
+        eb_app,
+        eb_template,
         ]
 
 if __name__ == "__main__":
