71ba65cc569fff000bf7aa2a35611e527a90d1c4
Chris Larsen <clarsen801@gmail.com>
2016-03-07 16:03:09 -0700

    remove calling resource name from exceptions.MissingOneOf. need to find a different way to implement this

--- fmc/exceptions.py
+++ fmc/exceptions.py
@@ -9,14 +9,12 @@ class MissingArgument(Exception):
                 )
 
 class MissingOneOf(Exception):
-    def __init__(self, missing_one_of, calling_class):
+    def __init__(self, missing_one_of):
         self.missing_one_of = ", ".join(missing_one_of)
-        self.calling_class = calling_class
 
     def __str__(self):
-        return "One of '{0}' Must Be Defined In {1}".format(
-                self.missing_one_of,
-                self.calling_class,
+        return "One of '{0}' Must Be Defined".format(
+                self.missing_one_of
                 )
 
 #! vim: ts=4 sw=4 ft=python expandtab:
