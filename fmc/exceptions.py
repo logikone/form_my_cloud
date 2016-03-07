88f3279b9e93c3789888740569750aedfad0a702
Chris Larsen <clarsen801@gmail.com>
2016-03-07 14:45:05 -0700

    add MissingOneOf exception

--- fmc/exceptions.py
+++ fmc/exceptions.py
@@ -8,4 +8,15 @@ class MissingArgument(Exception):
                 self.missing
                 )
 
+class MissingOneOf(Exception):
+    def __init__(self, missing_one_of, calling_class):
+        self.missing_one_of = ", ".join(missing_one_of)
+        self.calling_class = calling_class
+
+    def __str__(self):
+        return "One of '{0}' Must Be Defined In {1}".format(
+                self.missing_one_of,
+                self.calling_class,
+                )
+
 #! vim: ts=4 sw=4 ft=python expandtab:
