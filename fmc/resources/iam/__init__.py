3b381583e9e061ccbb7c48fa29fece19d14344c0
Chris Larsen <clarsen801@gmail.com>
2016-03-08 11:06:46 -0700

    import all sub resources in iam/__init__

--- fmc/resources/iam/__init__.py
+++ fmc/resources/iam/__init__.py
@@ -1,11 +1,19 @@
-from .role import Role
-from .policy import Policy
+from .access_key import AccessKey
+from .group import Group
 from .instance_profile import InstanceProfile
+from .managed_policy import ManagedPolicy
+from .policy import Policy
+from .role import Role
 from .user import User
+from .user_to_group_addition import UserToGroupAddition
 
 __all__ = [
+        AccessKey,
+        Group,
         InstanceProfile,
+        ManagedPolicy,
         Policy,
         Role,
         User,
+        UserToGroupAddition
         ]
