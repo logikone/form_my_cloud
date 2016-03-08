0c0ec08e26d4b716ae57328068d5ddd7feff6478
Chris Larsen <clarsen801@gmail.com>
2016-03-08 10:28:36 -0700

    add __init__ def to ResourceBase for inheriting classes to use

--- fmc/resources/base.py
+++ fmc/resources/base.py
@@ -1,8 +1,23 @@
 from fmc.base import Base
 
 class ResourceBase(Base):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.LogicalID = kwargs["LogicalID"]
+        self.Properties = kwargs["Properties"]
+        self.doc = {
+                "Resources": {
+                    self.LogicalID: {
+                        "Type": self.Type,
+                        "Properties": self.Properties
+                        }
+                    }
+                }
+
+        if len(self.Properties.keys()) == 0:
+            self.doc["Resources"][self.LogicalID].pop("Properties")
+
+        if "DependsOn" in kwargs:
+            self.doc["Resources"][self.LogicalID]["DependsOn"] = kwargs["DependsOn"]
 
     def __repr__(self):
         return "<{0}: {1}>".format(
@@ -12,3 +27,5 @@ class ResourceBase(Base):
 
     def representation(self):
         return self.doc
+
+#! vim: ts=4 sw=4 ft=python expandtab:
