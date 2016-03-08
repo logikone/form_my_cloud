91f43f612496fcd2f73a6c8d2c9222311fe19420
Chris Larsen <clarsen801@gmail.com>
2016-03-07 17:43:21 -0700

    add Required/Optional Properties decorators

--- fmc/decorators.py
+++ fmc/decorators.py
@@ -24,3 +24,29 @@ def RequiresOneOf(required_args):
             return func(*args, **kwargs)
         return wrapper
     return check_requirements
+
+def RequiredProperties(required):
+    def add_required_props(func):
+        def wrapper(*args, **kwargs):
+            kwargs["Properties"] = {}
+
+            for opt in required:
+                if opt not in kwargs:
+                    raise MissingArgument(opt)
+                else:
+                    kwargs["Properties"][opt] = kwargs[opt]
+            return func(*args, **kwargs)
+        return wrapper
+    return add_required_props
+
+def OptionalProperties(optional):
+    def add_optional_props(func):
+        def wrapper(*args, **kwargs):
+            kwargs["Properties"] = {}
+
+            for opt in optional:
+                if opt in kwargs:
+                    kwargs["Properties"][opt] = kwargs[opt]
+            return func(*args, **kwargs)
+        return wrapper
+    return add_optional_props
